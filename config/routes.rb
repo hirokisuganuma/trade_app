cbd571b22fda4a4ad16d87e3a12be343b561adb7
hirokisuganuma <40607473+hirokisuganuma@users.noreply.github.com>
2023-03-18 14:17:53 +0900

    Revert "Add fiscal schedule code"

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
-# frozen_string_literal: true
-
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # TODO: indexへ変更予定
-  root 'trading_value_high_rankings#show'
-  resources :year_to_date_highs, only: [:index]
-  resources :fiscal_schedule_codes, only: [:index]
+  # root "articles#index"
 end
